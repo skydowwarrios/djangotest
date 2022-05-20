1af8ceee34fca9e3d94a2e33337440c0c656f830
skydowwarrios <skydowwarrios@gmail.com>
2022-05-20 16:15:41 +0800

    add hello3.html and hello4.html

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
 
-# Create your views here.
+def hello(request):
+    return HttpResponse('<h1>Hello world!</h1>')
+    
+def hello2(request,username):
+    now = datetime.now()
+    return render(request,'hello3.html',
+                   {'username':username,'now':now})
--- test0506/urls.py
+++ test0506/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from myapp.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
+    path('', hello),
+    path('hello2/<str:username>', hello2),
 ]
